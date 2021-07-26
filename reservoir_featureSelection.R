0b9d30e939d4cd823d3bf5700aafc80229100326
Adithya Madhusoodanan <YOUR@EMAIL.com>
2021-07-27 00:59:55 +0530

    new merged dataset and code

--- reservoir_featureSelection.R
+++ reservoir_featureSelection.R
@@ -1,10 +1,4 @@
-"""
-Babayan, Orton & Streicker
-
-Predicting Reservoir Hosts and Arthropod Vectors from Evolutionary Signatures in RNA Virus Genomes
-
--- Genomic bias feature selection for reservoir host model
-"""
+setwd("~/Users/adithya/COVID-Reservoir-Prediciton")
 
 rm(list=ls())
 setwd("") # Set local working directory where files are located
@@ -19,55 +13,49 @@ library(matrixStats)
 
 localh20<-h2o.init(nthreads = -1)  # Start a local H2O cluster using nthreads = num available cores
 
-# Data
-f1<-read.csv(file="BabayanEtAl_VirusData.csv",header=T)
+# import and remove X and ID columns
 
-# Feature definition
-dinucs<-grep("[A|T|G|C|U]p[A|T|G|C|U]",names(f1),value=T)
-cps<-grep(".[A|C|D|E|F|G|H|I|K|L|M|N|P|Q|R|S|T|V|W|X|Y]..[A|T|G|C|U]",names(f1),value=T)
-aa.codon.bias<-grep(".Bias",names(f1),value=T)
+f1<-read.csv(file="GEN_TFIDF_Fourier-3.csv",header=T)
+f1 <- subset(f1, select = -c(X, ID)) # remove 
 
-f1<-f1[,c("Virus.name","Genbank.accession","Reservoir","Viral.group","Vector.borne","Vector",dinucs,cps,aa.codon.bias)]
-f1$response<-factor(f1$Reservoir)
+# renaming the reservoir to response and making it categorical
+f1$response <-factor(f1$Reservoir)
 
 # Remove orphans
 f2<-subset(f1,f1$response!="Orphan")
 f<-droplevels(f2)
 
 # Group selection based on sample size thresholds
+# Gropping any reservoir group with less than 15 sequences
+
 t<-15 # Minimum sample size per group
 s<-.7 # Proportion in the training set
 host.counts<-table(f$response)
-min.t<-host.counts[host.counts>=t] # minimum number of viruses per host group
+min.t <-host.counts[host.counts>=t] # minimum number of viruses per host group
 f_st3<-f[f$response %in% c(names(min.t)),]
-f_st3<-droplevels(f_st3)
+f_st3<-droplevels(f_st3) # remove empty spaces
 
 rm(f,f2,f1)
 
 # Evaluate patterns over many training sets
-set.seed(78910)
-nloops<-50
+set.seed(1000000)
+nloops<-1
 
-vimps<-matrix(nrow=length(c(cps,dinucs,aa.codon.bias))-2,ncol=nloops) # two are removed because ATG.Bias and TGG.Bias are constant
+vimps <-matrix(nrow=length(f_st3)-6-2, ncol=nloops) # create a m by n matrix where n number of rows (nloops) and m columns (number of features)
+# -2 because two features are constant and will be dropped
 
 for (i in 1:nloops){
-    trains<-f_st3 %>% group_by(response) %>%
+    trains <-f_st3 %>% group_by(response) %>%  # create the training set from 70% of total data
     filter(Genbank.accession %in% sample(unique(Genbank.accession), ceiling(s*length(unique(Genbank.accession)))))
     trains<-droplevels(trains)
-
-    set<-c("response",dinucs,cps,aa.codon.bias)
-    f1_train<-trains[,c(set)]
+    f1_train <- subset(trains, select = -c(Genbank.accession,Reservoir,Viral.group,Vector.borne,Vector))
     train<-as.h2o(f1_train)
-
-    # Identity the response column
+     # Identity the response column
     y <- "response"
-
     # Identify the predictor columns
     x <- setdiff(names(train), y)
-
     # Convert response to factor
     train[,y] <- as.factor(train[,y])
-
     # GBM with 5x cross validation of training set, test set is not used
     model1 <- h2o.gbm(x = x,
                       y = y,
@@ -77,10 +65,9 @@ for (i in 1:nloops){
                       sample_rate = 1,
                       max_depth = 10,
                       col_sample_rate_per_tree = 1,
-                      seed = 123,
+                      seed = 445,
                       nfolds = 5,
                       keep_cross_validation_predictions=T)
-
     # Retreive feature importance
     vi <- h2o.varimp(model1)
     data2  <- vi[order(vi[,1],decreasing=FALSE),] # order alphabetically
